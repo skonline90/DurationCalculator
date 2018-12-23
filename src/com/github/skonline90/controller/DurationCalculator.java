909711b025dae87a2d47d6fb26f56136fb9c5553
skonline90 <skonline90@posteo.mx>
2018-12-23 07:48:49 +0100

    Created Basic Project Structure

---
+++ src/com/github/skonline90/controller/DurationCalculator.java
@@ -0,0 +1,29 @@
+package com.github.skonline90.controller;
+
+import java.time.LocalDateTime;
+
+/**
+ * Use to calculate duration between two points in time
+ * or to calculate the date after a certain duration.
+ * 
+ * A duration is a amount of time. It can be measured in
+ * days, hours, minutes, seconds or combinations of 
+ * those units.
+ *
+ * @author skonline90
+ * @version 23.12.2018
+ */
+public class DurationCalculator
+{
+    public DurationCalculator()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public static long calculateSecondsBetweenTwoPointsInTime(LocalDateTime a, LocalDateTime b)
+    {
+        
+        
+        return 0;
+    }
+}
