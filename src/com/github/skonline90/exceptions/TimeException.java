2189a21dfe9338b545c5c9e71e560bbf9712140c
skonline90 <skonline90@posteo.mx>
2018-12-24 10:35:08 +0100

    Removed all problems from project

--- src/com/github/skonline90/exceptions/TimeException.java
+++ src/com/github/skonline90/exceptions/TimeException.java
@@ -2,6 +2,8 @@ package com.github.skonline90.exceptions;
 
 public class TimeException extends Exception
 {
+    private static final long serialVersionUID = 1L;
+
     public TimeException(String message)
     {
         super(message);
